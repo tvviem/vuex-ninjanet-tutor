d238d384611b0355a1c44a4b9360e5b1449c5918
viem trieu <trieuvinhviem@gmail.com>
2018-06-26 21:55:56 +0700

    Vuex DataStore - mapping actions and getters

--- src/components/ProductListOne.vue
+++ src/components/ProductListOne.vue
@@ -12,42 +12,40 @@
 </template>
 
 <script>
+import { mapActions } from "vuex";
+import { mapGetters } from "vuex";
 export default {
   computed: {
-      products(){
-          return this.$store.state.products
-      },
-      saleProducts() {
-          return this.$store.getters.saleProducts;
-      }
+    products() {
+      return this.$store.state.products;
+    },
+    ...mapGetters(["saleProducts"]) // using three dot operator in ES6
   },
   methods: {
-      reducePrice(amount) {
-        this.$store.dispatch('reducePrice', amount); // using mutation
-      }
+    ...mapActions(["reducePrice"])
   }
 };
 </script>
 
 <style scoped>
-#product-list-one{
-    background: #FFF8B1;
-    box-shadow: 1px 2px 3px rgba(0,0,0,0.2);
-    margin-bottom: 30px;
-    padding: 10px 20px;
+#product-list-one {
+  background: #fff8b1;
+  box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.2);
+  margin-bottom: 30px;
+  padding: 10px 20px;
 }
-#product-list-one ul{
-    padding: 0;
+#product-list-one ul {
+  padding: 0;
 }
-#product-list-one li{
-    display: inline-block;
-    margin-right: 10px;
-    margin-top: 10px;
-    padding: 20px;
-    background: rgba(255,255,255,0.7);
+#product-list-one li {
+  display: inline-block;
+  margin-right: 10px;
+  margin-top: 10px;
+  padding: 20px;
+  background: rgba(255, 255, 255, 0.7);
 }
-.price{
-    font-weight: bold;
-    color: #E8800C;
+.price {
+  font-weight: bold;
+  color: #e8800c;
 }
 </style>
